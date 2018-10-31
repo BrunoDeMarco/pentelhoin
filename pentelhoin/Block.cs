a69177c3362b7a50278b01553bbff8d2b0fc5758
Bruno Appolonio <bruno.appolonio@dextra-sw.com>
2018-10-31 15:34:29 -0300

    Commit inicial com a classe Block

---
+++ pentelhoin/Block.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace pentelhoin
+{
+    public class Block
+    {
+        private int Index { get; set; }
+        private string Data { get; set; }
+        private DateTime TimeStamp { get; set; }
+        private string Hash { get; set; }
+        private string PreviousHash { get; set; }
+
+        public Block(int index, string data, DateTime timestamp, string hash, string previousHash)
+        {
+            Index = index;
+            Data = data;
+            TimeStamp = timestamp;
+            Hash = hash;
+            PreviousHash = previousHash;
+        }
+    }
+}
