1ca9450c3746be6ee0ab4f3b703777890b7ebe7e
Bruno Appolonio <bruno.appolonio@dextra-sw.com>
2018-11-05 09:48:37 -0200

    Adicionando comeco de transacoes

---
+++ pentelhoin/Transaction.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace pentelhoin
+{
+    public class Transaction
+    {
+        public string FromAddress { get; set; }
+        public string ToAddress { get; set; }
+        public int Amount { get; set; }
+    }
+}
